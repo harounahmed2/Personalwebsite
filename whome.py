1a2d5f1d34d5aaf546c65824a10637de1f694878
Haroun Ahmed <hahmed@us.ibm.com>
2016-10-26 13:58:28 -0400

    First commit

---
+++ whome.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    author = "Fiasco"
+    name = "Big Nick Magnuson"
+    return render_template('index.html', author=author, name=name)
+
+if __name__ == '__main__':
+    app.run()
