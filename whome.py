ec9f6da182dc6f17387c1af5865220b641561b2c
Haroun Ahmed <hahmed@us.ibm.com>
2016-12-19 18:11:14 -0600

    Airline changes

--- whome.py
+++ whome.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from datetime import datetime
 import os
 import sys
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/homepage')
 def homepage():
     author = "HFiasco"
     name = "Kyle Souders"
-    return render_template('index.html', author=author, name=name)
+    return render_template('homepage.html', author=author, name=name)
 
 
 
