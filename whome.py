be345acd3d6a7452f9b9d5f51f55fa514572147c
Haroun Ahmed <hahmed@us.ibm.com>
2017-02-02 22:30:55 +0100

    Skeleton Base With Text Content

--- whome.py
+++ whome.py
@@ -22,6 +22,10 @@ def page_not_found(e):
     """Custom 500 Page."""
     return render_template('500Error.html'), 500
 
+@app.route('/resume')
+def resumemethod():
+    return redirect('as')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
